842fb65abe2ca4cc0d832122e118878a2882d30b
Munisa <munisaraxmonova04@gmail.com>
2026-02-06 15:23:46 +0500

    Initial commit: upload clinic_api project

--- connection.js
+++ connection.js
@@ -2,28 +2,11 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  host: "localhost",   
+  port: 5432,         
+  user: "postgres",   
+  password: "1234",    
+  database: "postgres", 
 });
 
 export default pool;
-
-
-
-// import { Pool } from "pg"
-
-// const pool = new Pool({
-//   host: "localhost",
-//   port: 5432,
-//   user: "postgres",
-//   password: "1234",
-//   database: "postgres",
-// });
-
-// export default pool;
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from 'express';
 import doctorRouter from './routes/doctorRoutes.js';
 import patientRouter from './routes/patientRoutes.js';
