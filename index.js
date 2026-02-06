ab4da276835aed384163345ccacc220d21a8219f
Munisa <munisaraxmonova04@gmail.com>
2026-02-06 16:58:39 +0500

    Remove auto-initialization - use manual dataset.sql setup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { initializeDatabase } from './initializeDB.js';
 import doctorRouter from './routes/doctorRoutes.js';
 import patientRouter from './routes/patientRoutes.js';
 import branchRouter from './routes/branchRoutes.js';
@@ -17,9 +16,6 @@ app.use('/branches', branchRouter);
 app.use('/appointments', appointmentRouter);
 app.use('/treatments', treatmentRouter);
 app.use('/payments', paymentRouter);
-
-// Initialize database on startup
-initializeDatabase();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
